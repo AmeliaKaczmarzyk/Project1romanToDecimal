c9ea1726b99a6274d657d40d24a78bc9bd024eaf
Marin Kitagawa <marinkitagawa.x86@gmail.com>
2022-10-10 08:34:12 -0400

    Made variable names tolerable

--- src/RomanToDecimal.java
+++ src/RomanToDecimal.java
@@ -1,38 +1,51 @@
 public class RomanToDecimal {
     public static int romanToDecimal(String roman) {
-        int sum = 0;
-        int num = 0;
-        int prevnum = 0;
+        int runningSum = 0;
+        int currentNumber;
+        int previousNumber = 0;
+        roman = roman.toUpperCase();
         for (int i = 0; i < roman.length(); i++) {
             char temp = roman.charAt(i);
             if (temp == 'I') {
-                num = 1;
+                currentNumber = 1;
             } else if (temp == 'V') {
-                num = 5;
+                currentNumber = 5;
             } else if (temp == 'X') {
-                num = 10;
+                currentNumber = 10;
             } else if (temp == 'L') {
-                num = 50;
+                currentNumber = 50;
             } else if (temp == 'C') {
-                num = 100;
+                currentNumber = 100;
             } else if (temp == 'D') {
-                num = 500;
+                currentNumber = 500;
             } else if (temp == 'M') {
-                num = 1000;
-            }
-            if (num > prevnum){
-                sum += num;
-                sum = sum - 2 * prevnum;
+                currentNumber = 1000;
             }
             else{
-                sum += num;
+                return -1;
             }
-            prevnum = num;
+            if (currentNumber > previousNumber){
+                runningSum += currentNumber;
+                runningSum = runningSum - 2 * previousNumber;
+            }
+            else{
+                runningSum += currentNumber;
+            }
+            previousNumber = currentNumber;
         }
-        return sum;
+        return runningSum;
     }
 
     public static void main(String[] args) {
-        System.out.println(romanToDecimal("MCDXCVI"));
+        for (String roman : args){
+            int decimal = romanToDecimal(roman);
+            if(decimal != -1){
+                System.out.println("input: " + roman + " => output " + decimal);
+            }
+            else{
+                System.out.println("input: " + roman + " => output => invalid"
+                );
+            }
+        }
     }
 }
